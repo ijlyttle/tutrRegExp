a60f7360cdf7c469a94079cbedca35939e877204
ijlyttle <ian.lyttle@schneider-electric.com>
2013-01-02 15:56:44 -0600

    Made a set of tests for logistics

---
+++ R/fn_00.R
@@ -0,0 +1,16 @@
+require(stringr)
+
+#' fn_00_01
+#'
+#' The user will modify the function to return a value according to the
+#' specification.
+#'
+#' @return  a regular expression that matches the sequence of characters "hat"
+#' 
+fn_00_01 <- function(){
+  
+  # replace the string below with "hat"
+  result <- "hat"
+  
+  return(result)
+}
---
+++ inst/tests/test-fn_00.R
@@ -0,0 +1,15 @@
+context("logistics")
+
+# For this sequence, you must modify the functions in the file "fn_00.R" such that
+# they each return a regular expression that meets the specifation of its tests. 
+
+# Modify the function fn_00_01 such that it returns a string describing a
+# regular expression that matches:
+#
+#   the sequence of characters "hat"
+#
+test_that("we match the sequence 'hat'", {
+  expect_that(str_count('I like that hat', fn_00_01()), equals(2))
+  expect_that(str_count('cat in the hat', fn_00_01()), equals(1))
+  expect_that(str_count('just a cat', fn_00_01()), equals(0))
+})
